88ef0d679bb54dd6274cf3ae45cbc8b24d8b951b
NurulHudaDev <nurulhuda22dev@gmail.com>
2022-04-04 16:20:26 +0300

    assignment-9

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navigation from './components/Navigation/Navigation.jsx';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home.jsx';
+import Reviews from './components/Reviews/Reviews.jsx';
+import Dashboard from './components/Dashboard/Dashboard.jsx';
+import Blog from './components/Blog/Blog.jsx';
+import About from './components/About/About.jsx';
+
+import NotFound from './components/NotFound/NotFound.jsx';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation></Navigation>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/reviews" element={<Reviews></Reviews>}></Route>
+        <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+        <Route path='/blog' element={<Blog></Blog>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
     </div>
   );
 }
